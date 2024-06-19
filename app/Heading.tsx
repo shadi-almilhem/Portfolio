a7dbac3009003e0e006b1ff4c92c97028b5eec38
Shadi Al Milhem <almilhemshadi@gmail.com>
2024-06-19 16:08:10 +0400

    header component edited

--- app/Heading.tsx
+++ app/Heading.tsx
@@ -16,23 +16,25 @@ type HeadingProps = {
 
 function Heading({ imagePlace }: HeadingProps) {
   return (
-    <h1
-      className={`text-glow gradient-text relative inline-block justify-center px-4 text-center font-bold ${bodoni_moda.className}`}
-      style={{ fontSize: "clamp(40px, 5vw, 88px)" }}
-    >
-      Shadi Al Milhem
+    <div className="relative">
       {imagePlace && (
-        <span className="absolute mx-2 md:-bottom-3 md:-right-28 lg:bottom-0 2xl:-right-32">
+        <span className="absolute mx-2 md:-bottom-3 md:-right-28 lg:-right-[7.5rem] lg:bottom-0 2xl:-right-32">
           <Image
             src={profileImage}
             quality={20}
             loading="lazy"
             alt="Shadi image"
-            className="image-gradient-border hidden h-auto rotate-3 rounded-md bg-contain md:flex md:w-28 md:rounded-xl"
+            className="image-gradient-border hidden h-auto rotate-3 rounded-md bg-contain md:flex md:w-24 md:rounded-xl lg:w-28"
           />
         </span>
       )}
-    </h1>
+      <h1
+        className={`text-glow gradient-text inline-block justify-center px-4 text-center font-bold ${bodoni_moda.className}`}
+        style={{ fontSize: "clamp(40px, 6vw, 88px)" }}
+      >
+        Shadi Al Milhem
+      </h1>
+    </div>
   );
 }
 
