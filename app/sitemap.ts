93ea87985789402b4fd11597dccc734a99becf8e
Shadi Al Milhem <almilhemshadi@gmail.com>
2024-06-19 13:02:24 +0400

    sitemap function

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://shadialmilhem.com/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://shadialmilhem.com/projects",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://shadialmilhem.com/services",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://shadialmilhem.com/contact",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://shadialmilhem.com/pdf/Shadi%20Al%20Milhem%20CV.pdf",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.2,
+    },
+  ];
+}
