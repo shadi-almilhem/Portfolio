abbbe74fd12d2c2a35d27d5437edad0f2ffb6bf7
Shadi Al Milhem <almilhemshadi@gmail.com>
2024-06-23 17:16:39 +0400

    dynamic import test

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "./Hero";
 import Projects from "./Projects";
-import Services from "./Services";
-import Stacks from "./Stacks";
+import dynamic from "next/dynamic";
 
-import RotatingGlow from "@/components/ui/RotatingGlow";
-import AboutMe from "./AboutMe";
 import { ProjectProvider } from "./projects/ProjectContext";
 import { Metadata } from "next";
+import { Skeleton } from "@/components/ui/skeleton";
 export const metadata: Metadata = {
   title: "Home | Shadi Al Milhem",
   description:
@@ -43,17 +41,52 @@ export const metadata: Metadata = {
     canonical: "https://shadialmilhem.com/",
   },
 };
+const DynamicServices = dynamic(() => import("./Services"), {
+  loading: () => (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  ),
+});
+const DynamicAboutMe = dynamic(() => import("./AboutMe"), {
+  loading: () => (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+    </div>
+  ),
+});
+const DynamicStack = dynamic(() => import("./Stacks"), {
+  loading: () => (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+    </div>
+  ),
+});
+const DynamicRotationGlow = dynamic(
+  () => import("@/components/ui/RotatingGlow"),
+  {
+    loading: () => (
+      <div className="flex flex-col space-y-3">
+        <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+      </div>
+    ),
+  },
+);
 export default function Home() {
   return (
     <ProjectProvider>
       <main className="  flex w-full  flex-col items-center justify-between gap-28 ">
         <Hero />
         <Projects />
-        <Services />
-        <AboutMe />
-        <Stacks />
+        <DynamicServices />
+        <DynamicAboutMe />
+        <DynamicStack />
 
-        <RotatingGlow />
+        <DynamicRotationGlow />
       </main>
     </ProjectProvider>
   );
